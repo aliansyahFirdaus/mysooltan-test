ad2ae97524fc4d5e2247ddfdab4158de1142624b
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-07-10 00:36:44 +0700

    done add redux for repos list user

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./user-slice";
+import repoReducer from "./repo-slice"
 
 const store = configureStore({
-  reducer: { userDetail: userReducer },
+  reducer: { userDetail: userReducer, repoList: repoReducer },
 });
 
 export default store;
