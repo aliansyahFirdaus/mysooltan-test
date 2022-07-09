374455bd6c52d9dfdec302686a33992d82b2ae69
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-07-09 12:49:49 +0700

    add css module

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
