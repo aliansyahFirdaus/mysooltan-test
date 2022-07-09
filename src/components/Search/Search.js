6a854604a3e567310609bee3d870f656372c3598
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-07-09 13:31:36 +0700

    add github logo on search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import { Container, Form, InputGroup } from "react-bootstrap";
+import { Container, Form, InputGroup, Stack } from "react-bootstrap";
 
 import React from "react";
 import styles from "./Search.module.css";
@@ -6,10 +6,16 @@ import styles from "./Search.module.css";
 export default function Search() {
   return (
     <Container className={styles.search}>
-      <InputGroup className="d-flex justify-content-center align-items-center">
-        <i className={`fa-solid fa-user ${styles.icon}`}></i>
-        <Form.Control type="text" />
-      </InputGroup>
+      <Stack direction="horizontal" gap={3}>
+        
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" />
+
+        <InputGroup className={styles.input}>
+          <i className={`fa-solid fa-at ${styles.icon}`}></i>
+          <Form.Control type="text" />
+        </InputGroup>
+
+      </Stack>
     </Container>
   );
 }
